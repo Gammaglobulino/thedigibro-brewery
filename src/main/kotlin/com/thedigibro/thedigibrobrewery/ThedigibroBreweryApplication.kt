6b562839cb01d7d56b94a0b297d4514348dd4ae4
DESKTOP-KCS1KCG\Drako <mazzantia@hotmail.com>
2019-09-08 15:34:54 +0200

    Initial commit

---
+++ src/main/kotlin/com/thedigibro/thedigibrobrewery/ThedigibroBreweryApplication.kt
@@ -0,0 +1,11 @@
+package com.thedigibro.thedigibrobrewery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ThedigibroBreweryApplication
+
+fun main(args: Array<String>) {
+	runApplication<ThedigibroBreweryApplication>(*args)
+}
